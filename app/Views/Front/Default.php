99ce8a57745e1c7d859b472066f3770ac0239f16
ivankozak <freeivanzw@gmail.com>
2024-07-06 20:59:31 +0300

    add favicon

--- app/Views/Front/Default.php
+++ app/Views/Front/Default.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyProject</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?=base_url('favicon/apple-touch-icon.png');?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?=base_url('favicon/favicon-32x32.png');?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?=base_url('favicon/favicon-16x16.png');?>">
+    <link rel="manifest" href="<?=base_url('favicon/site.webmanifest');?>">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="stylesheet" href="<?=base_url('css/bootstrap.min.css');?>">
     <link rel="stylesheet" href="<?=base_url('css/icons/font/bootstrap-icons.min.css');?>">
     <link rel="stylesheet" href="<?=base_url('css/base.css');?>">
